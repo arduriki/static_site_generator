4f1735103d0446b050b2cd43ebcb2e90bb1c05ef
arduriki <jordi.ardura.professional@gmail.com>
2025-01-28 11:43:05 +0100

    texttype enum and textnode class created

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode, TextType
+
+
+def main():
+    text_node = TextNode("This is a text node", TextType.BOLD_TEXT, "https://boot.dev")
+    print(text_node)
+
+
+if __name__ == "__main__":
+    main()
