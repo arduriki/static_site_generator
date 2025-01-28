0b7fe00fbc7afce0d847beed3ca9bc41a5c983cb
arduriki <jordi.ardura.professional@gmail.com>
2025-01-28 12:58:29 +0100

    leadnode and its test moved to their own files

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -39,24 +39,3 @@ class TestHTMLNode(unittest.TestCase):
         self.assertIsNone(parent_node.props)
         self.assertEqual(parent_node.children[0].value, "Hello, world!")
         self.assertEqual(parent_node.children[1].tag, "a")
-
-
-class TestLeafNode(unittest.TestCase):
-    def test_leaf_node_no_tag(self):
-        node = LeafNode(None, "Just some text")
-        self.assertEqual(node.to_html(), "Just some text")
-
-    def test_leaf_node_paragraph(self):
-        node = LeafNode("p", "This is a paragraph of text.")
-        self.assertEqual(node.to_html(), "<p>This is a paragraph of text.</p>")
-
-    def test_leaf_node_with_props(self):
-        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
-        self.assertEqual(
-            node.to_html(), '<a href="https://www.google.com">Click me!</a>'
-        )
-
-    def test_leaf_node_no_value(self):
-        node = LeafNode("p", None)
-        with self.assertRaises(ValueError):
-            node.to_html()
---
+++ src/test_leafnode.py
@@ -0,0 +1,24 @@
+import unittest
+
+from leafnode import LeafNode
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_node_no_tag(self):
+        node = LeafNode(None, "Just some text")
+        self.assertEqual(node.to_html(), "Just some text")
+
+    def test_leaf_node_paragraph(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        self.assertEqual(node.to_html(), "<p>This is a paragraph of text.</p>")
+
+    def test_leaf_node_with_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node.to_html(), '<a href="https://www.google.com">Click me!</a>'
+        )
+
+    def test_leaf_node_no_value(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
